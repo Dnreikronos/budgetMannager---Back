8e1e11600255742a49d773a7b974b55959be3017
Dnreikronos <dnreikronos2@gmail.com>
2024-08-21 23:06:43 -0300

    feat: insert more informations on the types for the API

--- types.go
+++ types.go
@@ -1,17 +1,44 @@
 package main
 
-import "golang.org/x/exp/rand"
+import (
+	"time"
+
+	"github.com/go-playground/locales/id"
+	"golang.org/x/exp/rand"
+)
 
 type User struct {
-	ID       int64
-	Email    string
-	Name     string
-	Password string
+	ID       		int 				`json:"id"`
+	Email    		string			`json:"email"`
+	Name     		string	 		`json:"name"`
+	Password 		string			`json:"password"`
+	Role 				string			`json:"role"`
+	IsActive		bool				`json:"is_active"`
+}
+
+type Budget struct {
+	ID 							int 					`json:"id"`	
+	Value						string 				`json:"value"`
+	UserID 					int 					`json:"userid"`
+	Currency    		string 				`json:"currency"`
+	Validity 				int						`json:"validity"`
+}
+
+type Bills struct {
+	ID  				int						`json:"id"`
+	Value 			string				`json:"value"`
+	Data				Data					`json:"data"`
+	UserID			int						`json:"userid"`
+	BudgetID		int						`json:"budgetid"`
+	Category 		string				`json:"category"`
+	Status 			string				`json:"status"`
+	CreatedAt 	time.Time			`json:"created_at"`
+	UpdatedAt		time.Time			`json:"updated_at"`
 }
 
 func newUser(Name, Email, Password string) *User {
 	return &User{
-		ID          int64  `json:"id"`
+		ID:       int64,
 		Email:    Email,
 		Name:     Name,
 		Password: Password,
