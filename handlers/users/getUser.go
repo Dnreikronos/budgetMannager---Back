32447544f4f38e9e478035b783c7d5a949c4bf83
Dnreikronos <dnreikronos2@gmail.com>
2024-08-27 22:22:59 -0300

    feat: create handler for get specific user

---
+++ handlers/users/getUser.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	models "github.com/Dnreikronos/budgetMannager---Back/models/users"
+	"github.com/go-chi/chi"
+)
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		log.Printf("Error at trying to do parse of id: %v", id)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	user, err := models.Get(int64(id))
+	if err != nil {
+		log.Printf("Error at trying to update register: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewDecoder(w).Encode(user)
+}
