c7d5b02b2fe992e722e2182633d453d31f3ea995
Dnreikronos <dnreikronos2@gmail.com>
2024-08-30 20:25:07 -0300

    feat: create the getBills.go model

---
+++ models/bills/getBills.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/Dnreikronos/budgetMannager---Back/db"
+
+func GetBills(id int64) (bills Bills, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+
+	defer conn.Close()
+
+	row := conn.QueryRow(`SELECT * FROM bills WHERE id=$1`, bills.ID)
+
+	err = row.Scan(&bills.ID, &bills.Value, &bills.UserID, &bills.BudgetID, &bills.Category, &bills.Status, &bills.CreatedAt, &bills.UpdatedAt)
+
+	return
+}
