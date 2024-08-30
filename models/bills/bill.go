cb3d370ba05a1ba72a146e7300a89e399998d812
Dnreikronos <dnreikronos2@gmail.com>
2024-08-29 21:58:17 -0300

    feat: create the type bills struct

--- models/bills/bill.go
+++ models/bills/bill.go
@@ -1,1 +1,14 @@
 package models
+
+import "time"
+
+type Bills struct {
+	ID        int64     `json:"id"`
+	Value     string    `json:"value"`
+	UserID    int       `json:"userid"`
+	BudgetID  int       `json:"budgetid"`
+	Category  string    `json:"category"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
