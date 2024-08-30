19b9aa043120b9433767e898f80ce7584fd8ba0a
Dnreikronos <dnreikronos2@gmail.com>
2024-08-29 22:14:17 -0300

    feat: create the getAllBudget.go model with same logic of getAllUsers.go

---
+++ models/budgets/getAllBudget.go
@@ -0,0 +1,27 @@
+package models
+
+import "github.com/Dnreikronos/budgetMannager---Back/db"
+
+func getAll() (budget []Budget, err error) {
+	conn, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+
+	defer conn.Close()
+
+	rows, err := conn.Query(`SELECT * FROM budget`)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		var budgets Budget
+		err = rows.Scan(&budgets.ID, &budgets.Value, &budgets.UserID, &budgets.Currency, &budgets.Validity)
+		if err != nil {
+			continue
+		}
+		budget = append(budget, budgets)
+	}
+
+	return
+}
