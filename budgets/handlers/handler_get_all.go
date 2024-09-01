28787ebc931056efc0437554900c397bd3348e66
Dnreikronos <dnreikronos2@gmail.com>
2024-09-01 10:48:02 -0300

    feat: create the handler get all for get all register of budgets from database

---
+++ budgets/handlers/handler_get_all.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Dnreikronos/budgetMannager---Back/budgets/models"
+)
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	budget, err := models.GetAll()
+	if err != nil {
+		log.Printf("Error at trying to request the register: %v", err)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(budget)
+}
