01730281f7c3c34a0cf8e28ecfa7c58366437bb8
Dnreikronos <dnreikronos2@gmail.com>
2024-09-05 21:32:26 -0300

    feat: create all routes for the handlers

--- cmd/main.go
+++ cmd/main.go
@@ -29,8 +29,22 @@ func main() {
 	r.Use(corsMiddleware.Handler)
 
 	r.Post("/createUser", handlers.CreateUser)
+	r.Get("/getAllUsers", handlers.GetAllUsers)
+	r.Get("/GetUser{id}", handlers.GetUser)
+	r.Put("/UpdateUser", handlers.UpdateUser)
+	r.Delete("/deleteUser", handlers.DeleteUser)
 
-	r.Post("/createBudget", handlers.UpdateBudget)
+	r.Post("/CreateBudget", handlers.CreateBudget)
+	r.Get("/GetAllBudget", handlers.GetAllBudget)
+	r.Get("/GetBudget{id}", handlers.GetBudget)
+	r.Put("/UpdateBudget", handlers.UpdateBudget)
+	r.Delete("/DeleteBudgets", handlers.DeleteBudget)
+
+	r.Post("/CreateBills", handlers.CreateBills)
+	r.Get("/GetAllBills", handlers.GetAllBills)
+	r.Get("/GetBills{id}", handlers.GetBills)
+	r.Put("/UpdateBills", handlers.UpdateBills)
+	r.Delete("/DeleteBills", handlers.DeleteBills)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
 }
