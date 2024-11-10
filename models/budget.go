3f3210995c1ac5a298cda8417944b3fbf4a00a2f
Dnreikronos <dnreikronos2@gmail.com>
2024-11-10 17:18:18 +0000

    refactor: adjust identation in budget struct

--- models/budget.go
+++ models/budget.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Budget struct {
-  ID       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;"`
-	Value    int64 `json:"value"`
+  ID       uuid.UUID    `json:"id" gorm:"type:uuid;primaryKey;"`
+	Value    int64        `json:"value"`
   UserID   uuid.UUID    `json:"user_id" gorm:"type:uuid;"`
 	User     User
-  Currency string `json:"currency"`
-	Validity int    `json:"validity"`
+  Currency string       `json:"currency"`
+	Validity int          `json:"validity"`
 }
 
 func (b *Budget) BeforeCreate(d *gorm.DB) (err error) {
