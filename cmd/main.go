e430b6ebce5991ee1c15393b1aee7b7be6c60dcb
Dnreikronos <dnreikronos2@gmail.com>
2024-11-16 13:40:35 +0000

    feat: create route to update bills

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,9 @@ import (
 	"net/http"
 	"os"
 	"time"
-	
 	"github.com/Dnreikronos/budgetMannager---Back/configs"
 	conn "github.com/Dnreikronos/budgetMannager---Back/db/connetion"
-	"github.com/Dnreikronos/budgetMannager---Back/handlers"
+	h "github.com/Dnreikronos/budgetMannager---Back/handlers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -50,12 +49,13 @@ func main() {
 		MaxAge:        12 * time.Hour,
 	}))
 
-	r.POST("/register", handlers.CreateUserHandler)
-	r.POST("/login", handlers.LoginHandler)
+	r.POST("/register", h.CreateUserHandler)
+	r.POST("/login", h.LoginHandler)
+	r.PUT("/Bill/:id", h.UpdateBillsHandler)
 
-  authorized := r.Group("/", handlers.AuthMiddleware())
+  authorized := r.Group("/", h.AuthMiddleware())
   {
-    authorized.GET("/profile", handlers.ProfileHandler)
+    authorized.GET("/profile", h.ProfileHandler)
   }
 
 	http.ListenAndServe(fmt.Sprintf(":%s", configs.GetServerPort()), r)
