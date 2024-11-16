fb407ae7ca02cebf11424172be9bcb7d52de5adf
Dnreikronos <dnreikronos2@gmail.com>
2024-11-16 13:41:05 +0000

    feat: create func for handle the update bills

--- handlers/bill_handler.go
+++ handlers/bill_handler.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-
 	"github.com/Dnreikronos/budgetMannager---Back/models"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -30,4 +29,42 @@ func CreateBillsHandler(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"status": "Failed to create Bills"})
 	}
 	c.JSON(http.StatusCreated, gin.H{"Bill": newBill})
+}
+
+
+func UpdateBillsHandler(c *gin.Context) {
+	var input models.BillInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Status": err.Error()})
+		return
+	}
+
+	billID := c.Param("id")
+	if billID == " " {
+		c.JSON(http.StatusBadRequest, gin.H{"Status": "Bill ID is required"})
+		return
+	}
+
+	db, ok := c.MustGet("db").(*gorm.DB)
+	if !ok {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": "Database connection error"})
+			return
+	}
+
+
+	var bill models.Bills
+	if err := db.First(&bill, "id = ?", billID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"Status": "Bill not found"})
+		return
+	}
+
+	bill.Value = input.Value
+	bill.Category = input.Category
+	bill.Status = input.Status
+
+	if err := db.Save(&bill).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Status": "Failed to update Bill"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"Bill": bill})
 }
