466e0923f79d33516123dd5e188a87e799ad0517
Dnreikronos <dnreikronos2@gmail.com>
2024-11-29 22:27:47 -0300

    feat: create func to handle the update budget manipulations

--- handlers/budget_handler.go
+++ handlers/budget_handler.go
@@ -31,3 +31,44 @@ func CreateBudgetHandler(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, gin.H{"Budget": newBudget})
 }
+
+func UpdateBudgetHandler(c *gin.Context) {
+	var input models.BudgetInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Status": err.Error()})
+		return
+	}
+
+	budgetID := c.Param("id")
+	if budgetID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"Status": "Budget ID is required"})
+		return
+	}
+
+	db, ok := c.MustGet("db").(*gorm.DB)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": "Database connection error"})
+		return
+	}
+
+	var budget models.Budget
+	if err := db.First(&budget, "id = ?", budgetID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"Status": "Budget not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"Status": "Failed to fetch Budget"})
+		}
+		return
+	}
+
+	budget.Value = input.Value
+	budget.Currency = input.Currency
+	budget.Start = input.Start
+	budget.End = input.End
+	if err := db.Save(&budget).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Status": "Failed to update Budget"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"Budget": budget})
+
+}
