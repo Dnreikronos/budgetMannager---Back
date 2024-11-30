a969bf1b09cedc2ce550aad065550353f354f980
Dnreikronos <dnreikronos2@gmail.com>
2024-11-29 21:35:20 -0300

    refactor: remove Validity and create Start and End time.Time for save the period of an budget

--- models/budget.go
+++ models/budget.go
@@ -1,20 +1,23 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Budget struct {
-  ID       uuid.UUID    `json:"id" gorm:"type:uuid;primaryKey;"`
-	Value    int64        `json:"value"`
-  UserID   uuid.UUID    `json:"user_id" gorm:"type:uuid;"`
+	ID       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey;"`
+	Value    int64     `json:"value"`
+	UserID   uuid.UUID `json:"user_id" gorm:"type:uuid;"`
 	User     User
-  Currency string       `json:"currency"`
-	Validity int          `json:"validity"`
+	Currency string    `json:"currency"`
+	Start    time.Time `json:"start"`
+	End      time.Time `json:"end"`
 }
 
 func (b *Budget) BeforeCreate(d *gorm.DB) (err error) {
-  b.ID = uuid.New()
-  return
+	b.ID = uuid.New()
+	return
 }
